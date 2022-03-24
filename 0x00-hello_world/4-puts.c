c158c62938c2aa3f3dc7e904a76c1576ada78b64
clintonic19 <clintonictendency@gmail.com>
2022-03-23 20:22:02 -0700

    C program to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  * Return 0: return the entery point int as 0
  */
 
-int main (void) {
-		puts("Programming is like building a multilingual puzzle\n");
+int main (void) 
+	{
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 	}
