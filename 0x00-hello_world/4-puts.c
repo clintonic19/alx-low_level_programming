65952405979928f76f4ec82f6ffd54b00a603b23
clintonic19 <clintonictendency@gmail.com>
2022-03-23 20:16:24 -0700

    C program to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
+/**
+ * To print the a function from puts (programming is like building a multilingual puzzle
+ * Using the puts function
+ * Return 0: return the entery point int as 0
+ */
+
 int main (void) {
 		puts("Programming is like building a multilingual puzzle\n");
-		return 0;
+		return (0);
 	}
