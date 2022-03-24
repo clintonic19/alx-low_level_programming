b409bd54d08f5c3a5ddd20fc3eb99c8f1eea35dc
clintonic19 <clintonictendency@gmail.com>
2022-03-23 21:36:43 -0700

    Function using printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * Using the printf function to print (with proper grammer, but the outcome is piece of art)
+ * Using the printf function
+ * Return 0: return the entery point int as 0
+ */
+
+int main (void)
+	{
+		printf( "with proper grammer, but the outcome is a piece of art\n");
+
+		return (0);
+	}	
