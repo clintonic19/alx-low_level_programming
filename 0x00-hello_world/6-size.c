cfafec33aa7c3ced5015920c644ee2a74d3707e4
clintonic19 <clintonictendency@gmail.com>
2022-03-24 04:56:43 -0700

    Program to print C file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char charType;
 long int longType;
 long long int longType2;
 
-printf("Size of char: %zu byte(s)\n", sizeof(charType));
-printf("Size of int: %zu byte(s)\n", sizeof(intType));
-printf("Size of float: %zu byte(s)\n", sizeof(floatType));
-printf("Size of long int: %zu byte(s)\n", sizeof(longType));
-printf("Size of long long int: %zu byte(s)\n", sizeof(longType2));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longType2));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
