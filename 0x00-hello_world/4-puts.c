c3b0fcb9cfd4a273231e5b62e6daff6eba4f2ae2
clintonic19 <clintonictendency@gmail.com>
2022-03-23 20:28:58 -0700

    C program to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #include<stdio.h>
 
 /**
@@ -8,6 +10,7 @@
 
 int main (void) 
 	{
-		puts("\"Programming is like building a multilingual puzzle");
+		puts( "\"Programming is like building a multilingual puzzle" );
+
 		return (0);
 	}
