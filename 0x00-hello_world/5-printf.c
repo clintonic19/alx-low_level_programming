89fed4cbd68b48cf8fa5562f266f5877061638a0
clintonic19 <clintonictendency@gmail.com>
2022-03-24 03:44:16 -0700

    Program to print a C file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,8 @@
  * Using the printf function
  * Return 0: return the entery point int as 0
  */
-
 int main (void)
-	{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-	}	
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
