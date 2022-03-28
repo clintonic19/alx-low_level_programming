a4fb59b1d506747cd50f7945f42b6bc2995ea5fb
clintonic19 <clintonictendency@gmail.com>
2022-03-28 02:18:55 -0700

    A program to print alphabets in C.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+    	
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	
+	_putchar('\n')
+}
